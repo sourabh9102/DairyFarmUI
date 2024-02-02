76cb9053b1ef7dacfa1222dff26154117faf3779
sourabh9102 <er.sourabhbhatt@gmail.com>
2024-02-02 17:20:39 +0530

    fetch image on product overview and some design

--- src/features/Product/ProductOverview.js
+++ src/features/Product/ProductOverview.js
@@ -1,12 +1,13 @@
 import { Disclosure, Tab } from "@headlessui/react";
 import { CiStar, CiHeart } from "react-icons/ci";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { FiMinus, FiPlus } from "react-icons/fi";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchAllProductByIdAsync, selectProductById } from "./productSlice";
-import imga from "../../images/01.jpg";
 import { Rating, Stack } from "@mui/material";
+import axios from "axios";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -16,8 +17,9 @@ export default function ProductOverview() {
   const dispatch = useDispatch();
   const params = useParams();
   const product = useSelector(selectProductById);
-  console.log("here is product", product);
+  const [productImages, setProductImages] = useState([]);
   const [isProductLoaded, setIsProductLoaded] = useState(false);
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
 
   useEffect(() => {
     dispatch(fetchAllProductByIdAsync(params.productId.toString()));
@@ -25,78 +27,75 @@ export default function ProductOverview() {
 
   useEffect(() => {
     if (product && product.length > 0) {
-      console.log("Product Data:", product);
-      console.log("Product Name:", product[0].name);
-      console.log("Product Price:", product[0].price);
-      console.log("img", product[0].imageSrc);
-      console.log(
-        "Product image path:",
-        `http://localhost:3000${product[0].imageSrc.replace(/^\.\.\//, "/")}`
-      );
       setIsProductLoaded(true);
     }
-  }, [product]);
+
+    axios
+      .get(`http://localhost:5001/auth/productImages/${params.productId}`)
+      .then((res) => {
+        if (res.data.success) {
+          const imagePaths = res.data.images.map(
+            (imageObject) => imageObject.image
+          );
+          setProductImages(imagePaths);
+          setSelectedImageIndex(0);
+        }
+      })
+      .catch((err) => {
+        console.log("Error:", err);
+      });
+  }, [params.productId, product]);
+
+  const handleImageClick = (index) => {
+    setSelectedImageIndex(index);
+  };
 
   if (!isProductLoaded) {
     return <div>Loading...</div>;
   }
 
+  const largeImageSrc = `http://localhost:3000/${productImages[
+    selectedImageIndex
+  ].replace(/^\.\.\//, "")}`;
+
   return (
     <div className="bg-white">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <div className="lg:grid lg:grid-cols-2 lg:gap-x-8 lg:items-start">
-          {/* Image gallery */}
-          <Tab.Group as="div" className="flex flex-col-reverse">
-            {/* Image selector */}
-            <div className="hidden mt-6 w-full max-w-2xl mx-auto sm:block lg:max-w-none">
-              <Tab.List className="grid grid-cols-4 gap-6">
-                {product[0].imageSrc && (
-                  <Tab
-                    key={product[0].id}
-                    className="relative h-24 bg-white rounded-md flex items-center justify-center text-sm font-medium uppercase text-gray-900 cursor-pointer hover:bg-gray-50 focus:outline-none focus:ring focus:ring-offset-4 focus:ring-opacity-50"
-                  >
-                    {({ selected }) => (
-                      <>
-                        <span className="sr-only">{product[0].name}</span>
-                        <span className="absolute inset-0 rounded-md overflow-hidden">
-                          <img
-                            src={`http://localhost:3000${product[0].imageSrc.replace(
-                              /^\.\.\//,
-                              "/"
-                            )}`}
-                            alt={product[0].imageAlt}
-                            className="w-full h-full object-center object-cover"
-                          />
-                        </span>
-                        <span
-                          className={classNames(
-                            selected ? "ring-indigo-500" : "ring-transparent",
-                            "absolute inset-0 rounded-md ring-2 ring-offset-2 pointer-events-none"
-                          )}
-                          aria-hidden="true"
-                        />
-                      </>
-                    )}
-                  </Tab>
-                )}
-              </Tab.List>
-            </div>
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
+        <div className="lg:flex lg:items-start lg:space-x-5">
+          {/* Large Image */}
+          {productImages.length > 0 && (
+            <div className="lg:w-2/3 lg:mr-8 relative mb-5">
+              <img
+                src={largeImageSrc}
+                alt={`Product Image ${selectedImageIndex + 1}`}
+                className="w-full h-full object-center object-cover rounded-lg mb-6 sm:mb-0"
+              />
 
-            <Tab.Panels className="w-full aspect-w-1 aspect-h-1">
-              {product[0].imageSrc && (
-                <Tab.Panel key={product[0].id}>
+              {/* Image gallery (conditionally rendered for screen size) */}
+              <div className="hidden md:flex md:flex-row gap-4 mt-4 mb-5 overflow-x-auto">
+                {productImages.map((image, index) => (
                   <img
-                    src={imga}
-                    alt={product[0].imageAlt}
-                    className="w-full h-full object-center object-cover sm:rounded-lg"
+                    key={index}
+                    onClick={() => handleImageClick(index)}
+                    src={`http://localhost:3000/${image.replace(
+                      /^\.\.\//,
+                      ""
+                    )}`}
+                    alt={`Product Image ${index + 1}`}
+                    className={`h-24 w-full object-cover rounded cursor-pointer ${
+                      selectedImageIndex === index
+                        ? "border-2 border-indigo-500"
+                        : ""
+                    }`}
+                    style={{ flexBasis: `calc(25% - 8px)` }}
                   />
-                </Tab.Panel>
-              )}
-            </Tab.Panels>
-          </Tab.Group>
+                ))}
+              </div>
+            </div>
+          )}
 
           {/* Product info */}
-          <div className="mt-10 px-4 sm:px-0 sm:mt-16 lg:mt-0">
+          <div className="lg:w-1/3">
             <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
               {product[0].name}
             </h1>
@@ -125,11 +124,15 @@ export default function ProductOverview() {
             </div>
 
             <form className="mt-6">
-              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-2">
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-4 md:grid-cols-2 sm:grid-cols-2 ">
                 <button
                   type="button"
                   className="w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-indigo-500"
                 >
+                  <AiOutlineShoppingCart
+                    className="h-6 w-6 flex-shrink-0 mr-2"
+                    aria-hidden="true"
+                  />{" "}
                   Add to Cart
                 </button>
                 <button
@@ -140,7 +143,7 @@ export default function ProductOverview() {
                     className="h-6 w-6 flex-shrink-0"
                     aria-hidden="true"
                   />
-                  {"  "} Add to Wishlist
+                  Add to Wishlist
                 </button>
               </div>
             </form>
--- src/features/Product/productSlice.js
+++ src/features/Product/productSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
 import { fetchAllProducts, fetchProductById } from "./productAPI";
 
 const initialState = {

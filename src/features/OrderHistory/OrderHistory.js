89d8c09754e84fdcd190daf8fafaaa4a3f99cc4a
sourabh9102 <er.sourabhbhatt@gmail.com>
2024-02-28 15:33:22 +0530

    Invoice in pdf format

--- src/features/OrderHistory/OrderHistory.js
+++ src/features/OrderHistory/OrderHistory.js
@@ -1,19 +1,20 @@
+import React, { useRef } from "react";
 import { useSelector } from "react-redux";
 import { SideBarNav } from "../UserProfile/UserProfile";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import ReactToPrint from "react-to-print";
 import { selectUserDetails } from "../Counter/userDetailsSlice";
 import Invoice from "../OrderSummary/Invoice";
 import { Link } from "react-router-dom";
+import { IoMdPrint } from "react-icons/io";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 export default function OrderHistory() {
   const userDetail = useSelector(selectUserDetails);
   const [orderDetails, setOrderDetails] = useState([]);
-  const componentRef = useRef();
-  const handlePrint = () => {
-    window.print();
-  };
+  const [selectedTrackingId, setSelectedTrackingId] = useState(null);
+  const pdfRef = useRef();
 
   useEffect(() => {
     const fetchOrderHistory = async () => {
@@ -26,7 +27,6 @@ export default function OrderHistory() {
         setOrderDetails(response.data.orderHistory);
       } catch (error) {
         console.error("Error fetching order history:", error);
-        setLoading(false);
       }
     };
     fetchOrderHistory();
@@ -37,6 +37,33 @@ export default function OrderHistory() {
     const options = { day: "2-digit", month: "2-digit", year: "numeric" };
     const formattedDate = date.toLocaleDateString("en-GB", options);
     return formattedDate;
+  };
+
+  const downloadInvoice = (trackingId) => {
+    setSelectedTrackingId(trackingId);
+    setTimeout(() => {
+      const input = pdfRef.current;
+      if (!input) {
+        console.error("Invoice component ref not found");
+        return;
+      }
+      const width = 1000;
+      const height = 1200;
+      html2canvas(input, { width, height }).then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const doc = new jsPDF("p", "mm", "a4", true);
+        const pdfWidth = doc.internal.pageSize.getWidth();
+        const pdfHeight = doc.internal.pageSize.getHeight();
+        const aspectRatio = canvas.width / canvas.height;
+        const imageWidth = pdfWidth;
+        const imageHeight = pdfWidth / aspectRatio;
+        const imageX = 0;
+        const imageY = 0;
+        doc.addImage(imgData, "PNG", imageX, imageY, imageWidth, imageHeight);
+        doc.save("invoice.pdf");
+        setSelectedTrackingId(null);
+      });
+    }, 1000);
   };
 
   return (
@@ -59,134 +86,158 @@ export default function OrderHistory() {
               <h2 className="sr-only">Recent orders</h2>
 
               <div className="space-y-20">
-                {orderDetails?.map((order) => (
-                  <div key={order.number}>
-                    <h3 className="sr-only">
-                      Order placed on{" "}
-                      <time dateTime={order.dateTime}>{order.dateTime}</time>
-                    </h3>
+                {orderDetails?.map((order, index) => {
+                  const showHeader =
+                    index === 0 ||
+                    order.trackingId !== orderDetails[index - 1].trackingId;
 
-                    <div className="bg-gray-50 rounded-lg py-6 px-4 sm:px-6 sm:flex sm:items-center sm:justify-between sm:space-x-6 lg:space-x-8">
-                      <dl className="divide-y divide-gray-200 space-y-6 text-sm text-gray-600 flex-auto sm:divide-y-0 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-x-6 lg:w-1/2 lg:flex-none lg:gap-x-8">
-                        <div className="flex justify-between sm:block">
-                          <dt className="font-medium text-gray-900">
-                            Date placed
-                          </dt>
-                          <dd className="sm:mt-1">
-                            <time dateTime={order.dateTime}>
-                              {formatDate(order.dateTime)}
-                            </time>
-                          </dd>
-                        </div>
+                  const ordersWithSameTrackingId = orderDetails.filter(
+                    (item) => item.trackingId === order.trackingId
+                  );
+                  const totalAmount = ordersWithSameTrackingId.reduce(
+                    (acc, curr) => acc + curr.total,
+                    0
+                  );
 
-                        <div className="flex justify-between pt-6 sm:block sm:pt-0">
-                          <dt className="font-medium text-gray-900">
-                            Order number
-                          </dt>
-                          <dd className="sm:mt-1">{order.orderId}</dd>
-                        </div>
-                        <div className="flex justify-between pt-6 font-medium text-gray-900 sm:block sm:pt-0">
-                          <dt>Total amount</dt>
-                          <dd className="sm:mt-1">{order.total}</dd>
-                        </div>
-                      </dl>
-                      <div className="flex sm:items-baseline sm:space-x-4 mt-5">
-                        <ReactToPrint
-                          trigger={() => (
+                  return (
+                    <div key={order.number}>
+                      {/* Display header only once for orders with the same trackingId */}
+                      {showHeader && (
+                        <div className="bg-gray-50 rounded-lg py-6 px-4 sm:px-6 sm:flex sm:items-center sm:justify-between sm:space-x-6 lg:space-x-8">
+                          <dl className="divide-y divide-gray-200 space-y-6 text-sm text-gray-600 flex-auto sm:divide-y-0 sm:space-y-0 sm:grid sm:grid-cols-3 sm:gap-x-6 lg:w-1/2 lg:flex-none lg:gap-x-8">
+                            <div className="flex justify-between sm:block">
+                              <dt className="font-medium text-gray-900">
+                                Date placed
+                              </dt>
+                              <dd className="sm:mt-1">
+                                <time dateTime={order.dateTime}>
+                                  {formatDate(order.dateTime)}
+                                </time>
+                              </dd>
+                            </div>
+
+                            <div className="flex justify-between pt-6 sm:block sm:pt-0">
+                              <dt className="font-medium text-gray-900">
+                                Order number
+                              </dt>
+                              <dd className="sm:mt-1">{order.orderId}</dd>
+                            </div>
+                            <div className="flex justify-between pt-6 font-medium text-gray-900 sm:block sm:pt-0">
+                              <dt>Total amount</dt>
+                              <dd className="sm:mt-1">{totalAmount}</dd>
+                            </div>
+                          </dl>
+                          <div className="flex sm:items-baseline sm:space-x-4 mt-5">
                             <button
-                              onClick={() => {
-                                const invoiceElement =
-                                  document.getElementById("invoice");
-                                if (invoiceElement) {
-                                  invoiceElement.classList.toggle("hidden");
-                                }
-                              }}
+                              onClick={() => downloadInvoice(order.trackingId)}
                               className="w-full flex items-center justify-center bg-white mt-6 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto sm:mt-0"
                             >
                               View invoice
                               <span aria-hidden="true"> &rarr;</span>
                             </button>
+                          </div>
+                          {selectedTrackingId === order.trackingId && (
+                            <div
+                              id={`invoice-${order.trackingId}`}
+                              className="rounded-lg border border-gray-500 p-4 mt-4"
+                            >
+                              <Invoice
+                                pdfRef={pdfRef}
+                                trackingId={selectedTrackingId}
+                              />
+                            </div>
                           )}
-                          content={() => componentRef.current}
-                        />
-                      </div>
-                      <div id="invoice" className="hidden" ref={componentRef}>
-                        <Invoice />
+                        </div>
+                      )}
+
+                      {/* Display products for the current trackingId */}
+                      <div className="rounded-lg border border-gray-500 p-4 mt-4">
+                        <table className="w-full text-gray-500">
+                          <caption className="sr-only">Products</caption>
+                          <thead className="sr-only text-sm font-medium text-gray-900 text-left sm:not-sr-only">
+                            <tr>
+                              <th
+                                scope="col"
+                                className="sm:w-2/5 lg:w-1/3 pr-8 py-3 font-normal"
+                              >
+                                Product
+                              </th>
+                              <th
+                                scope="col"
+                                className="hidden w-1/5 pr-8 py-3 font-normal sm:table-cell"
+                              >
+                                Price
+                              </th>
+                              <th
+                                scope="col"
+                                className="hidden pr-8 py-3 font-normal sm:table-cell"
+                              >
+                                Quantity
+                              </th>
+                              <th
+                                scope="col"
+                                className="hidden pr-8 py-3 font-normal sm:table-cell"
+                              >
+                                Status
+                              </th>
+                              <th
+                                scope="col"
+                                className="w-0 py-3 font-normal text-right"
+                              >
+                                Info
+                              </th>
+                            </tr>
+                          </thead>
+                          <tbody className="border-b border-gray-300 divide-y divide-gray-200 text-sm sm:border-t">
+                            <tr>
+                              <td className="py-6 pr-8">
+                                <div className="flex items-center">
+                                  <img
+                                    src={order.Product.imageSrc}
+                                    alt={order.Product.name}
+                                    className="w-16 h-16 object-center object-cover rounded mr-6"
+                                  />
+                                  <div>
+                                    <div className="font-medium text-gray-700">
+                                      {order.Product.name}
+                                    </div>
+                                    <div className="mt-1 sm:hidden">
+                                      {order.Product.price}
+                                    </div>
+                                  </div>
+                                </div>
+                              </td>
+                              <td className="hidden py-6 pr-8 sm:table-cell">
+                                {order.Product.price}
+                              </td>
+                              <td className="hidden py-6 pr-8 sm:table-cell">
+                                {order.quantity}
+                              </td>
+                              <td className="hidden py-6 pr-8 sm:table-cell">
+                                {order.status}
+                              </td>
+                              <td className="py-6 font-medium text-right whitespace-nowrap">
+                                <Link
+                                  to={`/productOverview/${order.Product.id}`}
+                                  className="text-indigo-600"
+                                >
+                                  View
+                                  <span className="hidden lg:inline">
+                                    {" "}
+                                    Product
+                                  </span>
+                                  <span className="sr-only">
+                                    , {order.Product.name}
+                                  </span>
+                                </Link>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
                       </div>
                     </div>
-
-                    <table className="mt-4 w-full text-gray-500 sm:mt-6">
-                      <caption className="sr-only">Products</caption>
-                      <thead className="sr-only text-sm font-medium text-gray-900 text-left sm:not-sr-only">
-                        <tr>
-                          <th
-                            scope="col"
-                            className="sm:w-2/5 lg:w-1/3 pr-8 py-3 font-normal"
-                          >
-                            Product
-                          </th>
-                          <th
-                            scope="col"
-                            className="hidden w-1/5 pr-8 py-3 font-normal sm:table-cell"
-                          >
-                            Price
-                          </th>
-                          <th
-                            scope="col"
-                            className="hidden pr-8 py-3 font-normal sm:table-cell"
-                          >
-                            Status
-                          </th>
-                          <th
-                            scope="col"
-                            className="w-0 py-3 font-normal text-right"
-                          >
-                            Info
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody className="border-b border-gray-200 divide-y divide-gray-200 text-sm sm:border-t">
-                        <tr>
-                          <td className="py-6 pr-8">
-                            <div className="flex items-center">
-                              <img
-                                src={order.Product.imageSrc}
-                                alt={order.Product.imageAlt}
-                                className="w-16 h-16 object-center object-cover rounded mr-6"
-                              />
-                              <div>
-                                <div className="font-medium text-gray-700">
-                                  {order.Product.name}
-                                </div>
-                                <div className="mt-1 sm:hidden">
-                                  {order.Product.price}
-                                </div>
-                              </div>
-                            </div>
-                          </td>
-                          <td className="hidden py-6 pr-8 sm:table-cell">
-                            {order.Product.price}
-                          </td>
-                          <td className="hidden py-6 pr-8 sm:table-cell">
-                            {order.status}
-                          </td>
-                          <td className="py-6 font-medium text-right whitespace-nowrap">
-                            <Link
-                              to={`/productOverview/${order.Product.id}`}
-                              className="text-indigo-600"
-                            >
-                              View
-                              <span className="hidden lg:inline"> Product</span>
-                              <span className="sr-only">
-                                , {order.Product.name}
-                              </span>
-                            </Link>
-                          </td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                ))}
+                  );
+                })}
               </div>
             </div>
           </div>
--- src/features/OrderSummary/Invoice.js
+++ src/features/OrderSummary/Invoice.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef, useEffect, useState } from "react";
 import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
 import {
   selectBillAddress,
@@ -6,8 +6,10 @@ import {
   selectProductDataDetails,
   selectSums,
 } from "../OrderDetails/orderDetailsSlice";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { SideBarNav } from "../UserProfile/UserProfile";
+import axios from "axios";
+import { selectUserDetails } from "../Counter/userDetailsSlice";
 
 const styles = StyleSheet.create({
   page: {
@@ -21,98 +23,161 @@ const styles = StyleSheet.create({
   },
 });
 
-const Invoice = (ref) => {
-  const orderDetail = useSelector(selectOrderDetails);
-  const productDataDetail = useSelector(selectProductDataDetails);
+const Invoice = forwardRef((props, ref) => {
+  const { trackingId, pdfRef } = props;
+  console.log("trackingId invoice me", trackingId);
+  const userDetail = useSelector(selectUserDetails);
   const billingAddress = useSelector(selectBillAddress);
   const sums = useSelector(selectSums);
+  const dispatch = useDispatch();
+  const [orderDetails, setOrderDetails] = useState([]);
+
+  useEffect(() => {
+    const fetchOrderHistory = async () => {
+      try {
+        const response = await axios.post(
+          "http://localhost:5001/auth/orderHistory",
+          {
+            userId: userDetail.id,
+          }
+        );
+        setOrderDetails(response.data.orderHistory);
+      } catch (error) {
+        console.error("Error fetching order history:", error);
+      }
+    };
+
+    fetchOrderHistory();
+  }, [trackingId, userDetail.id]);
+
+  const filteredOrderDetails = orderDetails.filter(
+    (order) => order.trackingId === trackingId
+  );
+
+  console.log("filteredOrderDetails", filteredOrderDetails);
+
   return (
     <>
-      <div className="sidebar-nav">
-        <SideBarNav />
-      </div>
-      <Document>
-        <Page size="A4" style={styles.page}>
-          <View style={styles.section}>
-            <div className="lg:pl-64 bg-white shadow-md rounded-lg overflow-hidden">
-              {/* Header */}
-              <div className="bg-gray-800 text-white py-4 px-6 flex justify-between items-center">
-                <div>
-                  <h1 className="text-2xl font-semibold">Dairy Delight</h1>
-                  <p>123 Street Name, City, Country</p>
-                  <p>Phone: +1234567890</p>
-                  <p>Email: info@example.com</p>
-                </div>
-                {/* <div>
-                <img src={logo} alt="Company Logo" className="h-12" />
-              </div> */}
-              </div>
-
-              {/* Customer information */}
-              <div className="py-4 px-6">
-                <h2 className="text-lg font-semibold">Billing Address</h2>
-                <p>
-                  {billingAddress[0].fname} {billingAddress[0].lname}
-                </p>
-                <p>{billingAddress[0].address}</p>
-                <p>{billingAddress[0].email}</p>
-              </div>
-
-              {/* Order details */}
-              <div className="border-t border-gray-200">
-                <h1 className="text-2xl px-6 py-4 bg-gray-100 font-semibold">
-                  Order Details
-                </h1>
-                {productDataDetail?.map((product, index) => (
-                  <div
-                    key={index}
-                    className="flex items-center justify-between py-2 px-6 border-b border-gray-200"
-                  >
+      <div ref={pdfRef}>
+        <Document>
+          <Page size="A4" style={styles.page}>
+            <View style={styles.section}>
+              {filteredOrderDetails?.map((order) => (
+                <div
+                  key={order.orderId}
+                  className="lg:pl-64 bg-white shadow-md rounded-lg overflow-hidden"
+                >
+                  {/* Header */}
+                  <div className="bg-gray-800 text-white py-4 px-6 flex justify-between items-center mt-48">
                     <div>
-                      <p className="text-lg font-semibold">{product.name}</p>
-                      <p>Quantity: {product.quantity}</p>
+                      <h1 className="text-2xl font-semibold">Dairy Delight</h1>
+                      <p>123 Street Name, City, Country</p>
+                      <p>Phone: +1234567890</p>
+                      <p>Email: info@example.com</p>
                     </div>
-                    <div>
-                      <p className="text-lg font-semibold">₹{product.price}</p>
+                    {/* <div>
+                          <img src={logo} alt="Company Logo" className="h-12" />
+                        </div> */}
+                  </div>
+
+                  {/* Customer information */}
+                  <div className="py-4 px-6">
+                    <h2 className="text-lg font-semibold">Billing Address</h2>
+                    {/* <p>
+                    {billingAddress[0].fname} {billingAddress[0].lname}
+                  </p>
+                  <p>{billingAddress[0].address}</p>
+                  <p>{billingAddress[0].email}</p> */}
+                  </div>
+
+                  {/* Order details */}
+                  <div className="border-t border-gray-200">
+                    <h1 className="text-2xl px-6 py-4 bg-gray-100 font-semibold">
+                      Order Details
+                    </h1>
+                    {filteredOrderDetails.map((order, index) => (
+                      <div
+                        key={index}
+                        className="flex items-center justify-between py-2 px-6 border-b border-gray-200"
+                      >
+                        <div>
+                          <p className="text-lg font-semibold">
+                            {order.Product.name}
+                          </p>
+                          <p>Quantity: {order.quantity}</p>
+                        </div>
+                        <div>
+                          <p className="text-lg font-semibold">
+                            ₹{order.total}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+
+                    {/* Total amount */}
+                    <div className="flex items-center justify-between py-2 px-6">
+                      <p className="font-semibold">Subtotal:</p>
+                      <p>
+                        ₹
+                        {filteredOrderDetails.reduce(
+                          (total, order) => total + order.subtotal,
+                          0
+                        )}
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between py-2 px-6">
+                      <p className="font-semibold">Shipping:</p>
+                      <p>
+                        ₹
+                        {filteredOrderDetails.reduce(
+                          (total, order) => total + order.shippingCost,
+                          0
+                        )}{" "}
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between py-2 px-6">
+                      <p className="font-semibold">Tax:</p>
+                      <p>
+                        ₹
+                        {filteredOrderDetails.reduce(
+                          (total, order) => total + order.tax,
+                          0
+                        )}
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between py-2 px-6">
+                      <p className="font-semibold">Total:</p>
+                      <p className="text-indigo-600">
+                        ₹
+                        {filteredOrderDetails.reduce(
+                          (total, order) => total + order.total,
+                          0
+                        )}
+                      </p>
                     </div>
                   </div>
-                ))}
-                {/* Total amount */}
-                <div className="flex items-center justify-between py-2 px-6">
-                  <p className="font-semibold">Subtotal:</p>
-                  <p>₹{sums?.totalSubtotal}</p>
-                </div>
-                <div className="flex items-center justify-between py-2 px-6">
-                  <p className="font-semibold">Shipping:</p>
-                  <p>₹{sums?.totalShippingCost}</p>
-                </div>
-                <div className="flex items-center justify-between py-2 px-6">
-                  <p className="font-semibold">Tax:</p>
-                  <p>₹{sums?.totalTax}</p>
-                </div>
-                <div className="flex items-center justify-between py-2 px-6">
-                  <p className="font-semibold">Total:</p>
-                  <p className="text-indigo-600">₹{sums?.totalTotal}</p>
-                </div>
-              </div>
 
-              {/* Payment information */}
-              <div className="py-4 px-6">
-                <h2 className="text-lg font-semibold">Payment Information</h2>
-                <p>Payment method: {orderDetail[0].paymentMethod}</p>
-              </div>
+                  {/* Payment information */}
+                  <div className="py-4 px-6">
+                    <h2 className="text-lg font-semibold">
+                      Payment Information
+                    </h2>
+                    <p>Payment method: {order.paymentMethod}</p>
+                  </div>
 
-              {/* Footer */}
-              <div className="bg-gray-800 text-white py-4 px-6 text-center">
-                <p>Thank you for your purchase!</p>
-              </div>
-            </div>
-          </View>
-        </Page>
-        <style>{`@media print {.sidebar-nav { display: none !important;}}`}</style>
-      </Document>
+                  {/* Footer */}
+                  <div className="bg-gray-800 text-white py-4 px-6 text-center mb-48">
+                    <p>Thank you for your purchase!</p>
+                  </div>
+                </div>
+              ))}
+            </View>
+          </Page>
+          <style>{`@media print {.sidebar-nav { display: none !important;}}`}</style>
+        </Document>
+      </div>
     </>
   );
-};
+});
 
 export default Invoice;
